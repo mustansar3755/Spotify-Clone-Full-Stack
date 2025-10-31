8961e42a08c32da768286424282c931caaa76d53
mustansar3755 <mustansar3755hussain@gmail.com>
2025-10-31 19:55:26 +0500

    Sidebar Section Done & update some assets data

---
+++ frontend/src/assets/assets.js
@@ -0,0 +1,184 @@
+import bell_icon from "./images/bell.png";
+import home_icon from "./images/home.png";
+import like_icon from "./images/like.png";
+import loop_icon from "./images/loop.png";
+import mic_icon from "./images/mic.png";
+import next_icon from "./images/next.png";
+import play_icon from "./images/play.png";
+import pause_icon from "./images/pause.png";
+import plays_icon from "./images/plays.png";
+import prev_icon from "./images/prev.png";
+import search_icon from "./images/search.png";
+import shuffle_icon from "./images/shuffle.png";
+import speaker_icon from "./images/speaker.png";
+import stack_icon from "./images/stack.png";
+import zoom_icon from "./images/zoom.png";
+import plus_icon from "./images/plus.png";
+import arrow_icon from "./images/arrow.png";
+import mini_player_icon from "./images/mini-player.png";
+import queue_icon from "./images/queue.png";
+import volume_icon from "./images/volume.png";
+import arrow_right from "./images/right_arrow.png";
+import arrow_left from "./images/left_arrow.png";
+import spotify_logo from "./images/Spotify_Logo.png";
+import clock_icon from "./images/clock_icon.png";
+import img1 from "./images/img1.jpg";
+import img2 from "./images/img2.jpg";
+import img3 from "./images/img3.jpg";
+import img4 from "./images/img4.jpg";
+import img5 from "./images/img5.jpg";
+import img6 from "./images/img6.jpg";
+import img7 from "./images/img7.jpg";
+import img8 from "./images/img8.jpg";
+import img9 from "./images/img9.jpg";
+import img10 from "./images/img10.jpg";
+import img11 from "./images/img11.jpg";
+import img12 from "./images/img12.jpg";
+import img13 from "./images/img13.jpg";
+import img14 from "./images/img14.jpg";
+import img15 from "./images/img15.jpg";
+import img16 from "./images/img16.jpg";
+import song1 from "./images/song1.mp3";
+import song2 from "./images/song2.mp3";
+import song3 from "./images/song3.mp3";
+
+export const assets = {
+  img6,
+  img13,
+  bell_icon,
+  home_icon,
+  like_icon,
+  loop_icon,
+  mic_icon,
+  next_icon,
+  play_icon,
+  plays_icon,
+  prev_icon,
+  search_icon,
+  shuffle_icon,
+  speaker_icon,
+  stack_icon,
+  zoom_icon,
+  plus_icon,
+  arrow_icon,
+  mini_player_icon,
+  volume_icon,
+  queue_icon,
+  pause_icon,
+  arrow_left,
+  arrow_right,
+  spotify_logo,
+  clock_icon,
+};
+
+export const albumsData = [
+  {
+    id: 0,
+    name: "Top 50 Global",
+    image: img8,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#2a4365",
+  },
+  {
+    id: 1,
+    name: "Top 50 India",
+    image: img9,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#22543d",
+  },
+  {
+    id: 2,
+    name: "Trending India",
+    image: img10,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#742a2a",
+  },
+  {
+    id: 3,
+    name: "Trending Global",
+    image: img16,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#44337a",
+  },
+  {
+    id: 4,
+    name: "Mega Hits",
+    image: img11,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#234e52",
+  },
+  {
+    id: 5,
+    name: "Happy Favorites",
+    image: img15,
+    desc: "Your weekly update of the most played tracks",
+    bgColor: "#744210",
+  },
+];
+
+export const songsData = [
+  {
+    id: 0,
+    name: "Song One",
+    image: img1,
+    file: song1,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "3:00",
+  },
+  {
+    id: 1,
+    name: "Song Two",
+    image: img2,
+    file: song2,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:20",
+  },
+  {
+    id: 2,
+    name: "Song Three",
+    image: img3,
+    file: song3,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:32",
+  },
+  {
+    id: 3,
+    name: "Song Four",
+    image: img4,
+    file: song1,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:50",
+  },
+  {
+    id: 4,
+    name: "Song Five",
+    image: img5,
+    file: song2,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "3:10",
+  },
+  {
+    id: 5,
+    name: "Song Six",
+    image: img14,
+    file: song3,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:45",
+  },
+  {
+    id: 6,
+    name: "Song Seven",
+    image: img7,
+    file: song1,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:18",
+  },
+  {
+    id: 7,
+    name: "Song Eight",
+    image: img12,
+    file: song2,
+    desc: "Put a smile on your face with these happy tunes",
+    duration: "2:35",
+  },
+];
---
+++ frontend/src/components/Sidebar.jsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { assets } from "../assets/assets";
+
+const SidebarCard = ({ title, desc, btnText }) => {
+  return (
+    <div className="p-4 bg-[#242424] m-2 rounded flex flex-col items-start justify-start gap-2 font-semibold pl-4">
+      <h1>{title}</h1>
+      <p className=" font-light">{desc}</p>
+      <button className=" px-4 py-1.5 bg-white text-lg rounded-full text-black mt-4 cursor-pointer">
+        {btnText}
+      </button>
+    </div>
+  );
+};
+const Sidebar = () => {
+  return (
+    <div className=" w-[25%] h-full hidden lg:flex flex-col gap-2 text-white">
+      <div className="bg-secondary h-[15%] rounded flex flex-col justify-around">
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className=" w-10" src={assets.home_icon} alt="" />
+          <p className=" font-bold">Home</p>
+        </div>
+        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+          <img className=" w-10" src={assets.search_icon} alt="" />
+          <p className=" font-bold">Search</p>
+        </div>
+      </div>
+      {/* Lower Section */}
+      <div className=" h-[85%] rounded bg-secondary">
+        <div className="p-4 flex items-center justify-between">
+          <div className="flex items-center gap-3">
+            <img className=" w-8" src={assets.stack_icon} alt="" />
+            <p>Your Library</p>
+          </div>
+          <div className=" flex items-center gap-3">
+            <img className=" w-5" src={assets.arrow_icon} alt="" />
+            <img className=" w-5" src={assets.plus_icon} alt="" />
+          </div>
+        </div>
+        {/* Sidebar Card */}
+        <SidebarCard
+          title="Create your first playlist"
+          desc="it's easy we'll help you"
+          btnText="Create Playlist"
+        />
+        <SidebarCard
+          title="Let's find some podcasts to follow"
+          desc="We'll keep you update on new episode"
+          btnText="Browse podcast"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
