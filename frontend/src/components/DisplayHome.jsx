10d2d71c40310e69ac073d594d3d2b49a94ed292
mustansar3755 <mustansar3755hussain@gmail.com>
2025-11-01 03:11:42 +0500

    Songs Data Display

--- frontend/src/components/DisplayHome.jsx
+++ frontend/src/components/DisplayHome.jsx
@@ -1,7 +1,8 @@
 import React from "react";
 import Navbar from "./Navbar";
-import { albumsData } from "../assets/assets";
+import { albumsData, songsData } from "../assets/assets";
 import AlbumItems from "./AlbumItems";
+import SongsItem from "./SongsItem";
 
 const DisplayHome = () => {
   return (
@@ -15,7 +16,7 @@ const DisplayHome = () => {
               key={index}
               name={item.name}
               desc={item.desc}
-            //   id={item.id}
+              id={item.id}
               image={item.image}
             />
           ))}
@@ -25,7 +26,9 @@ const DisplayHome = () => {
       <div className=" mb-4">
         <h1 className=" font-bold my-5 text-2xl">Today's biggest hitsrt's</h1>
         <div className=" flex overflow-auto">
-          {}
+          {songsData.map((item,index)=>(
+            <SongsItem key={index} name={item.name} desc={item.desc} image={item.image}/>
+          ))}
         </div>
       </div>
     </>
