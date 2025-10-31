5af6439a87c3bec505834751459a9b6c6cc807a9
mustansar3755 <mustansar3755hussain@gmail.com>
2025-11-01 03:50:43 +0500

    Display Album Dynamically With Songs Data

--- frontend/src/components/Display.jsx
+++ frontend/src/components/Display.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Route, Routes } from "react-router-dom";
 import DisplayHome from "./DisplayHome";
+import DisplayAlbum from "./DisplayAlbum";
 
 const Display = () => {
   return (
     <div className=" w-full lg:w-[75%] lg:ml-0 overflow-auto text-white m-2 px-6 pt-4 rounded bg-secondary">
       <Routes>
         <Route path="/" element={<DisplayHome />} />
+        <Route path="/album/:id" element={<DisplayAlbum />} />
       </Routes>
     </div>
   );
