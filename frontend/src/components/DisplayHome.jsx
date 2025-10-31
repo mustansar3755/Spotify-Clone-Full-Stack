f25f507cb38575c70d3ab1eea6c5ee6e7e3bdc92
mustansar3755 <mustansar3755hussain@gmail.com>
2025-10-31 21:35:04 +0500

    Albums Cards In DisplayHome Component

--- frontend/src/components/DisplayHome.jsx
+++ frontend/src/components/DisplayHome.jsx
@@ -1,12 +1,35 @@
-import React from 'react'
-import Navbar from './Navbar'
+import React from "react";
+import Navbar from "./Navbar";
+import { albumsData } from "../assets/assets";
+import AlbumItems from "./AlbumItems";
 
 const DisplayHome = () => {
   return (
     <>
-<Navbar/>
+      <Navbar />
+      <div className=" mb-4">
+        <h1 className=" font-bold my-5 text-2xl">Featured Chart's</h1>
+        <div className=" flex overflow-auto">
+          {albumsData.map((item, index) => (
+            <AlbumItems
+              key={index}
+              name={item.name}
+              desc={item.desc}
+            //   id={item.id}
+              image={item.image}
+            />
+          ))}
+        </div>
+      </div>
+      {/* Songs Data */}
+      <div className=" mb-4">
+        <h1 className=" font-bold my-5 text-2xl">Today's biggest hitsrt's</h1>
+        <div className=" flex overflow-auto">
+          {}
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default DisplayHome
+export default DisplayHome;
