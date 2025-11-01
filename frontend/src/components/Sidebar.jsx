39826ec60b64dc095595fbf4a030d5f567d0f8a2
mustansar3755 <mustansar3755hussain@gmail.com>
2025-11-01 23:25:36 +0500

    Back to Home Route

--- frontend/src/components/Sidebar.jsx
+++ frontend/src/components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { assets } from "../assets/assets";
-
+import {useNavigate} from 'react-router-dom'
 const SidebarCard = ({ title, desc, btnText }) => {
   return (
     <div className="p-4 bg-[#242424] m-2 rounded flex flex-col items-start justify-start gap-2 font-semibold pl-4">
@@ -13,10 +13,11 @@ const SidebarCard = ({ title, desc, btnText }) => {
   );
 };
 const Sidebar = () => {
+  const navigate = useNavigate()
   return (
     <div className=" w-[25%] h-full hidden lg:flex flex-col gap-2 text-white">
       <div className="bg-secondary h-[15%] rounded flex flex-col justify-around">
-        <div className="flex items-center gap-3 pl-8 cursor-pointer">
+        <div onClick={()=>navigate('/')} className="flex items-center gap-3 pl-8 cursor-pointer">
           <img className=" w-10" src={assets.home_icon} alt="" />
           <p className=" font-bold">Home</p>
         </div>
