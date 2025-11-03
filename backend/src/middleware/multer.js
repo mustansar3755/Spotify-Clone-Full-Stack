e495c9123e0401949542cd89538c5425c694022e
mustansar3755 <mustansar3755hussain@gmail.com>
2025-11-03 11:34:50 +0500

    middleware file update

--- backend/src/middleware/multer.js
+++ backend/src/middleware/multer.js
@@ -1,37 +1,48 @@
+// import multer from "multer";
+// import path from "path";
+
+// // Configure storage
+// const storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, "uploads/"); // files temporarily stored here
+//   },
+//   filename: function (req, file, cb) {
+//     const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
+//     cb(null, uniqueSuffix + path.extname(file.originalname)); // unique file name
+//   },
+// });
+
+// // File filter to allow only images and audio
+// const fileFilter = (req, file, cb) => {
+//   if (
+//     file.mimetype.startsWith("image/") ||
+//     file.mimetype.startsWith("audio/") ||
+//     file.mimetype.startsWith("video/") // optional if songs are video type
+//   ) {
+//     cb(null, true);
+//   } else {
+//     cb(new Error("Only image and audio files are allowed!"), false);
+//   }
+// };
+
+// // Multer instance
+// const upload = multer({
+//   storage,
+//   fileFilter,
+//   limits: {
+//     fileSize: 50 * 1024 * 1024, // 50MB limit
+//   },
+// });
+
+// export default upload;
+
 import multer from "multer";
-import path from "path";
 
-// Configure storage
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, "uploads/"); // files temporarily stored here
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    cb(null, uniqueSuffix + path.extname(file.originalname)); // unique file name
-  },
-});
-
-// File filter to allow only images and audio
-const fileFilter = (req, file, cb) => {
-  if (
-    file.mimetype.startsWith("image/") ||
-    file.mimetype.startsWith("audio/") ||
-    file.mimetype.startsWith("video/") // optional if songs are video type
-  ) {
-    cb(null, true);
-  } else {
-    cb(new Error("Only image and audio files are allowed!"), false);
+  filename:function(req,file,callback){
+    callback(null,file.originalname);
   }
-};
-
-// Multer instance
-const upload = multer({
-  storage,
-  fileFilter,
-  limits: {
-    fileSize: 50 * 1024 * 1024, // 50MB limit
-  },
-});
+})
+const upload = multer({storage});
 
 export default upload;
